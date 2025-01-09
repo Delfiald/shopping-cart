9047a2995ae68b4515c53a99e1f8a87d0f7baf20
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2025-01-09 13:50:52 +0700

    Add fetch functions, error handling, and validation
    - Added `getCategories` and `getProductsBasedOnCategory` functions
    - Added data structure validation for API responses
    - Implemented error handling for various scenarios (404, 500, network failure, etc.)

--- src/services/fakeStoreAPI.js
+++ src/services/fakeStoreAPI.js
@@ -1,12 +1,40 @@
 const BASE_URL = "https://fakestoreapi.com/products";
 
+const structureCheck = (data) => {
+ const checkProductStructure = (product) => {
+  if (!("id" in product)) {
+   console.error("Invalid response structure: Missing 'id' field");
+   throw new Error(
+    "Sorry, we couldn't fetch the product details. Please try again later."
+   );
+  }
+ };
+
+ const allProducts = () => {
+  if (Array.isArray(data)) {
+   data.forEach(checkProductStructure);
+  } else {
+   checkProductStructure(data);
+  }
+ };
+
+ return {
+  allProducts,
+ };
+};
+
 const fakeStoreAPI = async (request) => {
  const response = await fetch(`${request}`);
  if (!response.ok) {
-  throw new Error("Failed to fetch Data");
+  throw new Error(
+   `Failed to fetch data. HTTP Status: ${response.status} - ${
+    response.statusText || "No status text available"
+   }.`
+  );
  }
 
  const data = await response.json();
+
  return data;
 };
 
@@ -15,6 +43,7 @@ export const fetchData = (setError, setLoading) => {
   setLoading(true);
   try {
    const products = await fakeStoreAPI(BASE_URL);
+   structureCheck(products).allProducts();
    return products;
   } catch (error) {
    setError(error.message);
@@ -28,6 +57,7 @@ export const fetchData = (setError, setLoading) => {
   setLoading(true);
   try {
    const product = await fakeStoreAPI(`${BASE_URL}/${id}`);
+   structureCheck(product).allProducts();
    return product;
   } catch (error) {
    setError(error.message);
@@ -37,8 +67,36 @@ export const fetchData = (setError, setLoading) => {
   }
  };
 
+ const getProductsCategory = async () => {
+  setLoading(true);
+  try {
+   const categories = await fakeStoreAPI(`${BASE_URL}/categories`);
+   return categories;
+  } catch (error) {
+   setError(error.message);
+  } finally {
+   setLoading(false);
+  }
+ };
+
+ const getProductsBasedOnCategory = async (category) => {
+  setLoading(true);
+  try {
+   const products = await fakeStoreAPI(`${BASE_URL}/category/${category}`);
+   structureCheck(products).allProducts();
+
+   return products;
+  } catch (error) {
+   setError(error.message);
+  } finally {
+   setLoading(false);
+  }
+ };
+
  return {
   getAllProducts,
   getProductById,
+  getProductsCategory,
+  getProductsBasedOnCategory,
  };
 };
